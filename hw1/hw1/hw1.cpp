3529181a22570e64fa719ec17facd39e5401f0b7
yym36100 <yakabmarci@freemail.hu>
2015-06-23 21:50:49 +0300

    removed the stdafx stuff

--- hw1/hw1/hw1.cpp
+++ hw1/hw1/hw1.cpp
@@ -1,10 +1,8 @@
 // hw1.cpp : Defines the entry point for the console application.
 //
+#include <stdio.h>
 
-#include "stdafx.h"
-
-
-int _tmain(int argc, _TCHAR* argv[])
+int main(int argc, char* argv[])
 {
 	printf("Hello World\n");
 	return 0;
